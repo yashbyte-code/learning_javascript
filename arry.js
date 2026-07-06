8b6f4906ce492fe49c648c0aa7f2b9e6f3a1b413
Yash Sabale <yashsabale5550@gmail.com>
2026-07-06 12:55:15 +0530

    first save iin arry

---
+++ arry.js
@@ -0,0 +1,13 @@
+let arr = [1,2,3,4,5];
+console.log(arr)
+
+console.log(arr.at(2))  //at takes negative index also 
+
+
+const newarr = arr
+console.log(newarr)
+
+//splice in java script 
+console.log(arr.splice(1,5))
+console.log(arr.splice(1,4,true))
+console.log(arr)
