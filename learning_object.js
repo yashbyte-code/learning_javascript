1671a2cae9160f157efd2d0b551580c7c4164d8e
Yash Sabale <yashsabale5550@gmail.com>
2026-07-08 18:50:21 +0530

    method-1 of creating object

---
+++ learning_object.js
@@ -0,0 +1,11 @@
+// creating object in javascript 
+// there are 3 methods to create object 
+// method : 1
+
+const obj = {
+    name : "Yash",
+    Age : 20,
+    Branch :  "IT"
+}
+
+console.log(obj);
