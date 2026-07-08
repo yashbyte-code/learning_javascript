b8f43149388bc13933c3f30118971fe668a226d3
Yash Sabale <yashsabale5550@gmail.com>
2026-07-08 19:08:51 +0530

    data retrive from object done

--- learning_object.js
+++ learning_object.js
@@ -9,3 +9,23 @@ const obj = {
 }
 
 console.log(obj);
+
+// we can retrive data from object using key name 
+console.log(obj.name);
+console.log(obj.Age);
+
+// similarly we can use [] to retrive data from object 
+console.log(obj["name"]);
+console.log(obj["Age"]);
+
+
+// in object we can give number as a key name as below 
+const obj1 = {
+    0 : "Yash",
+    1 : "Sabale",
+    3 : 20
+}
+
+// to retrive data from this type of object we use [] not . and there is no need to write number in " " simply write in []
+console.log(obj1[0]);
+console.log(obj1[1]);
