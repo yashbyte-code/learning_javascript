1f894ecb5ea7162797d5768c2535a9d58c291582
Yash Sabale <yashsabale5550@gmail.com>
2026-07-11 15:26:48 +0530

    fuction creation done

---
+++ function.js
@@ -0,0 +1,11 @@
+// Function : function is a block of code executes only when we call it 
+// in javascript we can create function with 3 different methods as below
+
+// method 1)
+// creating function 
+function greet(){
+    console.log("kaise ho mere bhai Yash..");
+}
+
+// calling function
+greet();
