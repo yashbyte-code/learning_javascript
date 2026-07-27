483efaf19fae24e1ac065b2fb67479278e0dc02d
Yash Sabale <yashsabale5550@gmail.com>
2026-07-27 19:18:05 +0530

    accessing elements is done using tag name

--- DOM.js
+++ DOM.js
@@ -2,6 +2,7 @@
 // now  i learn accessing elements through css 
 // in javascript if there are multiple elements having sa,e class and 
 // i use query selector then it works on only element who is first 
+// to slect all elements containing same class name then we use "queryselectorAll" and it gives node list as an output
 
 const head1 = document.querySelector('#first_head');
 head1.innerHTML =  "Thoo Kaise Hai Aap loggg...";
@@ -9,3 +10,26 @@ head1.innerHTML =  "Thoo Kaise Hai Aap loggg...";
 let head2 = document.querySelector('#second_head')
 head2.innerHTML ="Aap Padh Chuke Ho..."
 
+head2.style.backgroundColor = "pink"
+
+// to iterate on node list then we can use for loop, forEach loop, for of loop 
+const node_list = document.querySelectorAll('.header_1');
+
+ for(let i of node_list)
+ {
+    console.log(i);
+ }
+
+
+//  forEach loop
+forEach((val)=>{
+    console.log(val);
+}
+
+)
+
+
+
+// accessing element by tag  name 
+const value = document.getElementsByTagName('h1');
+// suppos ein my html multiple elements are having same tag name so it will returns all values in form of htmlcollection 
