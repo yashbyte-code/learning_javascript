8aedb4bd4d5a50a91a4106541e0637c52ced6f75
Yash Sabale <yashsabale5550@gmail.com>
2026-08-07 17:24:15 +0530

    changinh color is done

--- basic_calculator/first.js
+++ basic_calculator/first.js
@@ -6,3 +6,35 @@ button.addEventListener("click", () => {
 
     document.querySelector("h2").innerText = "Result: " + (num1 + num2);
 });
+
+const colors = [
+  "aliceblue",
+  "antiquewhite",
+  "lightcoral",
+  "lightblue",
+  "lightgreen",
+  "lavender",
+  "beige",
+  "mistyrose",
+  "honeydew",
+  "peachpuff",
+  "thistle",
+  "powderblue",
+  "wheat",
+  "lightyellow",
+  "plum",
+  "#f0f8ff",
+  "#faebd7",
+  "#e6e6fa",
+  "#ffe4e1",
+  "#f5f5dc"
+];
+
+button.addEventListener('mousemove', ()=>{
+    const index1 = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[index1];
+   
+})
+
+setInterval(changing_bg, 3000);
+
